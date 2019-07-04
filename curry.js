440744cb3bff6ed935903ff5e86216228f1a019b
Eugeniy <mudrik1@mail.ru>
2019-07-04 17:48:10 +0300

    Update curry.js

--- curry.js
+++ curry.js
@@ -1,3 +1,7 @@
+/**
+  * 04.07.2019 osa911
+  * curry() return call after get all arguments
+ */
 export const curry = function(fn) {
   return function fn1(...args) {
     if (args.length >= fn.length) {
@@ -10,3 +14,9 @@ export const curry = function(fn) {
     }
   }
 }
+
+const summ = (a, b, c) => a + b + c;
+
+const summerC = curry(summ);
+
+console.log(summerC(1)(1,3)); // => 5
